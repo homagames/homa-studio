37db16efff6af973d42de69d785304bea1ee37fc
Sam Goddard <hello@samgoddard.co.uk>
2022-10-03 10:40:04 +0100

    preview mode wireup

--- utils/resolveProductionUrl.js
+++ utils/resolveProductionUrl.js
@@ -1,15 +1,22 @@
 // All Slugs go here...
 const globalSlugs = {
   home: '/',
-  work: '/work',
-  // etc: '/etc'
+  blog: '/blog',
+  games: '/games',
+  caseStudies: '/games',
+  about: '/about',
+  careers: '/careers',
+  homaLab: '/homa-lab',
+  products: '/homa-lab',
+  community: '/community',
+  privacy: '/privacy',
 }
 
 const secret = process.env.SANITY_STUDIO_SANITY_PREVIEW_SECRET
 export const getGlobalSlug = (slug) => globalSlugs[slug]
 
 // Change remote url...
-const remoteURL = 'https://your-live-site.com'
+const remoteURL = 'https://homa.vercel.app'
 const localURL = 'http://localhost:3000'
 export const previewURL = window.location.hostname === 'localhost' ? localURL : remoteURL
 
@@ -20,7 +27,7 @@ export const createUrl = ({ slug, globalSlug }) => {
   }
   let path = `${globalSlug}`
   if (slug) path += `/${slug.current}`
-  return `${previewURL}/api/preview?secret=${secret}&slug=${path}`
+  return `${previewURL}/api/preview?secret=skX5idURmxw1hHJytBgWMI0szSbDWzpIhnPCThSzMWM6PWMh16t9tXNEJlWVugeGYCI60ITccuOVv0Decrznfv0Ijb6xzlBhVg0fETTGNweee3kIZmbxI3dmSoaYvwojjyEDHbWz21kdSnwa3JLVCnQlIyhjvy65Ek3pXlPuXOrKHimP1LE8&slug=${path}`
 }
 
 export default function resolveProductionUrl (document) {
